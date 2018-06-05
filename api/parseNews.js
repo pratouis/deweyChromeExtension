18760fa9c0c2d8195abc9f373a7921285bf2121b
Andrew Marten Medina <ammy2015@mymail.pomona.edu>
2018-06-05 10:40:37 -0700

    still cleaning up

--- api/parseNews.js
+++ api/parseNews.js
@@ -20,7 +20,6 @@ module.exports = {
     router.use('/associated-articles', async (req, res, next) => {
       /* if no title is specified return 400 (user error)*/
       if (!req.query.title && !req.body.title) return res.status(400).json({success: false, error: 'no title provided'});
-      // console.log(req.query.title);
       let title = req.query.title.split('|')[0].trim();
       try {
         const { data } = await retext().use(retext_keywords).process(title)
@@ -40,10 +39,14 @@ module.exports = {
         const query = req.body.keywords.reduce((acc, term) => (acc ? `${acc} "${term}"` : `"${term}"`), "");
         const key = req.body.keywords.join('_');
         let data = JSON.parse(await db.getAsync(`${key}`)); // check if search already exists
-        if(!data){
+        if (!data) {
           const query = req.body.keywords.reduce((acc, term) => (acc ? `${acc} "${term}"` : `"${term}"`), "");
-          let { articles } = await newsapi.v2.everything({ q: query, language: 'en', sortBy: 'relevancy'});
-          if(!!!articles.length) throw "0 articles returned from newsapi";
+          let { articles } = await newsapi.v2.everything({
+            q: query,
+            language: 'en',
+            sortBy: 'relevancy'
+          });
+          if (!!!articles.length) throw "0 articles returned from newsapi";
           data = Object.values(
             _.mapObject( _.groupBy(articles, (article) => article.title.toLowerCase()), // group articles by title (not case-sensitive)
               (articles, title) => ({
@@ -51,11 +54,18 @@ module.exports = {
                 title: articles[0].title,
                 url: articles[0].url,
                 publishedAt: articles[0].publishedAt,
-                description: articles[0].description })))
-                .slice(0,5);  // take first five articles, TODO: make it so users can adjust the number of articles returned
+                description: articles[0].description
+              })
+            )
+          ).slice(0,5);  // take first five articles, TODO: make it so users can adjust the number of articles returned
           // set key-value to have 60*60*24 seconds to live where key is a string of space separated keywords in quotes
           const saved = await db.setexAsync(key, 86400, JSON.stringify(data));
-          if(saved !== 'OK'){ return res.status(500).json({ success: false, error: `from REDIS got: ${saved}`}); }
+          if (saved !== 'OK') {
+            return res.status(500).json({
+              success: false,
+              error: `from REDIS got: ${saved}`
+            });
+          }
         }
         res.status(200).json({ success: true, data });
       } catch(error) {
