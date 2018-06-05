64ac331e3a7544496c67cc6b6582eb084de32b45
Andrew Marten Medina <ammy2015@mymail.pomona.edu>
2018-06-05 10:22:19 -0700

    revised parseNews

--- api/parseNews.js
+++ api/parseNews.js
@@ -1,14 +1,11 @@
 import express from 'express';
 const router = new express.Router();
-
 import redis from 'redis';
 import bluebird from 'bluebird';
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 const db = redis.createClient();
-
 const _ = require('underscore');
-
 const retext = require('retext');
 const retext_keywords = require('retext-keywords');
 const nlcstToString = require('nlcst-to-string');
@@ -16,39 +13,30 @@ const nlcstToString = require('nlcst-to-string');
 /* API
 * returns success - boolean
 *         status code - 200 (OK), 400 (missing title), 500 (something went wrong in API)
-*
 */
 module.exports = {
   articleRouter: function (newsapi) {
     /* middleware parsing keywords */
     router.use('/associated-articles', async (req, res, next) => {
       /* if no title is specified return 400 (user error)*/
-      if(!req.query.title && !req.body.title){
-        return res.status(400).json({success: false, error: 'no title provided'});
-      }
+      if (!req.query.title && !req.body.title) return res.status(400).json({success: false, error: 'no title provided'});
       // console.log(req.query.title);
-      var title = req.query.title.split('|')[0].trim();
+      let title = req.query.title.split('|')[0].trim();
       try {
         const { data } = await retext().use(retext_keywords).process(title)
         req.body.keywords = data.keyphrases.length ?
           data.keyphrases.map( (phrase) => phrase.matches[0].nodes.map(nlcstToString).join('') ) :
           data.keywords.map( (keyword) => nlcstToString(keyword.matches[0].node) );
-          // console.log(req.body.keywords);
         next();
       } catch(err) {
-        console.error('error from retext in /newsapi: ',err);
         return res.status(500).send(err);
       }
     });
 
-    router.get('/associated-articles/redditTexts', async (req,res) => {
-
-      res.json({title: req.query.title, keywords: req.body.keywords});
-    });
+    router.get('/associated-articles/redditTexts', async (req,res) => res.json({title: req.query.title, keywords: req.body.keywords}));
 
     router.get('/associated-articles/byTitle', async (req, res) => {
       try {
-        // console.log(`title: ${req.query.title}\nkeywords: ${req.body.keywords}`);
         const query = req.body.keywords.reduce((acc, term) => (acc ? `${acc} "${term}"` : `"${term}"`), "");
         const key = req.body.keywords.join('_');
         // console.log('key: ', key);
